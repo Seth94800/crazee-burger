31d6779d3e4d62cca26dd47dc6ea531709a6d96f
Seth94800 <seth94800@gmail.com>
2023-08-03 18:35:25 +0200

    ✨ F01 add loginPage without css js

---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,30 @@
+
+
+export default function LoginPage() {
+
+     {/* state (état, datas) */}
+
+
+
+     {/* comportements */}
+
+
+     {/* affichage (render) */}
+  return (
+    <>
+    <h1>Bienvenue chez nous !</h1>
+    <h2>Connectez-vous</h2>
+    <form>
+        <input placeholder="entrez votre prénom..." required></input>
+        <button>Accédez à votre espace</button>
+    </form>
+   
+
+
+     
+    
+    
+    
+    </>
+  )
+}
