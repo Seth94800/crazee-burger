24c69aabaa87adb09eee2eaaa71d04b604d75ad7
Seth94800 <seth94800@gmail.com>
2023-08-12 20:05:05 +0200

    🚧 F04: ORDER > layout add container

--- src/components/Pages/order/OrderPage.jsx
+++ src/components/Pages/order/OrderPage.jsx
@@ -1,4 +1,9 @@
 import { Link, useParams } from "react-router-dom";
+import Logo from "../../reusable-ui/Logo";
+import "./OrderPage.css";
+import { BsPersonCircle } from "react-icons/bs";
+
+
 
 export default function OrderPage() {
     //state
@@ -7,11 +12,19 @@ const { username } = useParams()
 
     //affichage
   return (
-    <div>
-        <h1>Bonjour {username}</h1>
-        <Link to= "/">
-        <button>Déconnexion</button>
-        </Link>
+    <div className="container">
+      <div className="navbar">
+        <div className="logo-order">
+          <Logo />
+        </div>
+        <div className="nav-profil-container">
+          <h1>Hey, {username}</h1>
+          <Link to= "/">
+          <button>Se déconnecter</button>
+          </Link>
+          <BsPersonCircle className="bsperson" />
+        </div>
+      </div >
     </div>
   )
 }
--- src/components/reusable-ui/Logo.jsx
+++ src/components/reusable-ui/Logo.jsx
@@ -1,10 +1,10 @@
 
 export default function Logo() {
   return (
-    <div className="Logo">
-    <p>Crazee</p>
-    <img src="../src/assets/images/F03 logo-orange.png" alt="Logo" />
-    <p>Burger</p>
+    <div className="crazee-logo">
+    <p className="p-logo">Crazee</p>
+    <img className="img-logo" src="../src/assets/images/F03 logo-orange.png" alt="" />
+    <p className="p-logo">Burger</p>
   </div>
   )
 }
